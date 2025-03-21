bd742c257de9cb3ba20a57819810819bafaeb1e5
moses-25 <moses.ochieng1@student.moringaschool.com>
2025-03-21 17:02:38 +0300

    Add files via upload
    added multiple content in html , css and html

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiraku Ramen", image: "https://i.pinimg.com/474x/26/f1/19/26f119326fc93d7b4a387c3b4dedb75a.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Naruto's Favourite", image: "https://i.pinimg.com/236x/10/fb/b5/10fbb5f46b9e4a6b66fd73927c700eb5.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen Ichiban", image: "https://i.pinimg.com/236x/db/07/2d/db072d20bc9a2de134c1a39b52f320a3.jpg", rating: null, comment: null }
-    { id: 4, name: "Shio Ramen", restaurant: "Tokyo Ramen House", image:"https://moringa.instructure.com/courses/967/files/517801/preview"}
-    { id: 5, name: "Spicy Miso Ramen", restaurant: "Sapporo Ramen", image:"https://moringa.instructure.com/courses/967/files/517799/preview"}
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "https://i.pinimg.com/474x/26/f1/19/26f119326fc93d7b4a387c3b4dedb75a.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "https://i.pinimg.com/236x/10/fb/b5/10fbb5f46b9e4a6b66fd73927c700eb5.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "https://i.pinimg.com/236x/db/07/2d/db072d20bc9a2de134c1a39b52f320a3.jpg", rating: 3, comment: "too salty" }
 ];
 
 // Function to display ramen images with names
@@ -55,19 +53,18 @@ function addSubmitListener() {
         
         const newRamen = {
             id: ramens.length + 1,
-            name: document.getElementById('name').value.trim(),
-            restaurant: document.getElementById('restaurant').value.trim(),
-            image: document.getElementById('image').value.trim(),
-            rating: parseInt(document.getElementById('rating').value, 10) || null,
-            comment: document.getElementById('comment').value.trim() || null
+            name: document.getElementById('name').value,
+            restaurant: document.getElementById('restaurant').value,
+            image: document.getElementById('image').value,
+            rating: document.getElementById('rating').value,
+            comment: document.getElementById('comment').value
         };
-        
 
         ramens.push(newRamen);
         displayRamens();
         form.reset();
         handleClick(newRamen);
-        form.style.display = 'none';  // Hide the form after submission
+        document.getElementById('new-ramen-form').style.display = 'none';  // Hide the form after submission
     });
 }
 
